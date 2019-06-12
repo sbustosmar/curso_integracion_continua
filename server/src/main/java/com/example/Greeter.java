a5d2be2b555c7e7127ad19d1e999292764d9d862
sbustos <50901915+sbustosmar@users.noreply.github.com>
2019-06-12 10:05:33 +0200

    Update Greeter.java
    Se añade javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
   
+  /**  
+  /* greet method
+  /* input: someone
+  /* return string
+  * **/
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
